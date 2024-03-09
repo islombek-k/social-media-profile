6618f795d27f3f68821662a8c35e4ce2772d0fd1
islombek-k <islomaliy74@gmail.com>
2024-03-09 22:56:49 +0500

    feat: added images and refactor ProfileHeader

--- src/components/profile-header/ProfileHeader.tsx
+++ src/components/profile-header/ProfileHeader.tsx
@@ -1,8 +1,18 @@
 const ProfileHeader = () => {
   return (
     <div>
-      <img src="../../assets/banner.png" alt="banner" />
-      <img src="../../assets/profile-img.png" alt="profile" />
+      <img
+        src="assets/banner.png"
+        alt="banner"
+        className="relative w-full h-52"
+      />
+      <img
+        src="assets/profile-img.png"
+        alt="profile"
+        height={100}
+        width={100}
+        className="absolute top-33 left-0 right-0 m-auto"
+      />
       test
     </div>
   );
